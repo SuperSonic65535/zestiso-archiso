0ff55de4c71131774c757cb51de9e560657a1d95
dankcuddlybear <dankcuddlybear@proton.me>
2025-12-18 19:30:26 +0000

    General stability improvements for optimised user experience

--- zestiso-kde-gaming/profiledef.sh
+++ zestiso-kde-gaming/profiledef.sh
@@ -18,4 +18,6 @@ airootfs_image_type="squashfs"
 AIROOTFS_THREADS=16
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M' '-not-reproducible' '-processors' $AIROOTFS_THREADS)
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
-file_permissions=()
+file_permissions=(
+    ["/etc/dotfiles.d/live/Desktop/install-zestiso.desktop"]="0:0:755"
+)
